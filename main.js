72ab47991cef4fd32d75823be4d52347e38d93d9
denis360 <gomezdenis150@gmail.com>
2023-05-27 21:31:23 -0600

    final project | done

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
-var getdata =  function(){
+const obtenerDatos = () => {
+	let angulo = parseInt(document.getElementById("angulo").value);
+	const tiempo = parseInt(document.getElementById("tiempo").value);
+	const radio = parseInt(document.getElementById("radio").value);
+	const alert = document.getElementById("alert");
 
-    var angulo = document.getElementById("angulo").value;
-    var rapidez = document.getElementById("rapidez").value;
-    var tiempo = document.getElementById("tiempo").value;
-    var velocidadtangencial = document.getElementById("velocidadtangencial").value;
-    var longitudarco = document.getElementById("longitudarco").value;
-    var diametro = document.getElementById("diametro").value;
-    var periodo = document.getElementById("periodo").value;
-    var acecentripeta = document.getElementById("acecentripeta").value;
-    var frecuencia = document.getElementById("frecuencia").value;   
 
-    if(angulo === '' && rapidez === ''){
-        alert('Campo vacio');
-    }else{
-        alert('La velocidad angular es : ')
-    }
+	if (!angulo || !tiempo || !radio) {
+		alert.classList.remove("d-none");
+		return;
+	} else {
+		alert.classList.add("d-none");
+
+		angulo = angulo * (Math.PI / 180);
+
+		return { angulo, tiempo, radio }
+	}
 }
+const calcularDatos = (datos) => {
+	const { angulo, tiempo, radio } = datos;
 
+	const velocidadAngular = angulo/tiempo;
+	const velocidadTangencial = velocidadAngular * radio;
+	const aceleracionCentripeta = Math.pow(velocidadAngular, 2) * radio;
 
+	return {
+		velocidadAngular,
+		velocidadTangencial,
+		aceleracionCentripeta
+	}
+}
+(() => {
+	const button = document.getElementById("calcular");
+	const tabla = document.getElementById("tabla");
+	button.onclick = (e) => {
+		e.preventDefault();
+		const tr = document.createElement("tr");
+		const { velocidadAngular, velocidadTangencial, aceleracionCentripeta } = calcularDatos(obtenerDatos());
+		tr.innerHTML = `
+			<td>${velocidadAngular.toFixed(2)} rad/seg</td>
+			<td>${velocidadTangencial.toFixed(2)} m/s</td>
+			<td>${aceleracionCentripeta.toFixed(2)} m/s²</td>
+		`
+
+		tabla.appendChild(tr);
+	}
+})();
